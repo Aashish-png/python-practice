aa768139ea90f3efff4d8f02598352b924e460dc
aashish <aashishsarwa512@gmail.com>
2021-09-23 22:34:52 +0530

    first commit

---
+++ char_input.py
@@ -0,0 +1,11 @@
+
+name = input("entr your name ")
+print("your name is "+ name)
+
+age= input("enter your age ")
+age= int(age)
+
+age2 = 100-age
+age2= str(age2)
+
+print("you will be 100 years in "+age2  + " years")
